c7f1812b6fc9d371881bc883a9cf601825d19763
Red Lhota <rose.as.romeo@gmail.com>
2015-12-17 17:41:33 -0600

    Added CRUD actions to Rules Pages.

--- cms/app/controllers/rules_pages_controller.rb
+++ cms/app/controllers/rules_pages_controller.rb
@@ -1,2 +1,26 @@
 class RulesPagesController < ApplicationController
+  def index
+    @rules_pages = RulesPage.all
+  end
+  def show
+    @rules_page = RulesPage.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @rules_page = RulesPage.new(rules_page_params);
+
+    if @rules_page.save
+      redirect_to @rules_page
+    else
+      render 'new'
+    end
+  end
 end
+
+private
+  def rules_page_params
+    params.require(:rules_page).permit(:title, :text)
+  end
