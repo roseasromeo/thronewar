d6c37a07971acf8032ca7f8dbb4d6f877edd0acd
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2015-12-17 17:41:33 -0600

    Added CRUD actions to Rules Pages.

---
+++ cms/db/migrate/20151217224728_create_rules_pages.rb
@@ -0,0 +1,10 @@
+class CreateRulesPages < ActiveRecord::Migration
+  def change
+    create_table :rules_pages do |t|
+      t.string :title
+      t.text :text
+
+      t.timestamps null: false
+    end
+  end
+end
