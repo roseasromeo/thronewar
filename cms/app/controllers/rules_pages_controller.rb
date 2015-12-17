d3eb9f9cc8411ab2d4f96f96b9cdc046b7451c73
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2015-12-17 16:37:39 -0600

    Deleted pages. Created rules_pages for consistent labeling.

---
+++ cms/app/controllers/rules_pages_controller.rb
@@ -0,0 +1,2 @@
+class RulesPagesController < ApplicationController
+end
--- cms/test/controllers/rules_pages_controller_test.rb
+++ cms/test/controllers/rules_pages_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PagesControllerTest < ActionController::TestCase
+class RulesPagesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
