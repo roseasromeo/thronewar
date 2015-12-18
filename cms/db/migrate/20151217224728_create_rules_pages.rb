bbc6727281e4c4ef9fee1ba9492a2b0bb2c00295
Red Lhota <rose.as.romeo@gmail.com>
2015-12-18 14:43:18 -0600

    Added slugs for urls.

--- cms/db/migrate/20151217224728_create_rules_pages.rb
+++ cms/db/migrate/20151217224728_create_rules_pages.rb
@@ -2,6 +2,7 @@ class CreateRulesPages < ActiveRecord::Migration
   def change
     create_table :rules_pages do |t|
       t.string :name
+      t.string :slug, :default => "1"
       t.string :title
       t.text :text
 
