cf67af90a53c0075b17652fa39d15c0989224d8b
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2015-12-17 20:00:23 -0600

    Added hierarchy to RulesPage and Subpage. Nested routes.

--- cms/app/models/subpage.rb
+++ cms/app/models/subpage.rb
@@ -1,3 +1,5 @@
 class Subpage < ActiveRecord::Base
   belongs_to :rules_page
+  has_one :sidebar
+  has_one :body
 end
--- cms/config/routes.rb
+++ cms/config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
   get 'welcome/index'
 
-  resources :rules_pages
+  # Shallowly nest the routes
+  resources :rules_pages, shallow: true do
+    resources :subpages do
+      resources :sidebar
+      resources :body
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
