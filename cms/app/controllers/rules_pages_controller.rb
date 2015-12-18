033bd79d8b240aa897a53c8d3fb60a206ac486fc
Red Lhota <rose.as.romeo@gmail.com>
2015-12-17 18:40:06 -0600

    Added edit and update methods for Rules Pages.

--- cms/app/controllers/rules_pages_controller.rb
+++ cms/app/controllers/rules_pages_controller.rb
@@ -2,6 +2,7 @@ class RulesPagesController < ApplicationController
   def index
     @rules_pages = RulesPage.all
   end
+  
   def show
     @rules_page = RulesPage.find(params[:id])
   end
@@ -11,11 +12,11 @@ class RulesPagesController < ApplicationController
   end
 
   def edit
-    @rules_page = Article.find(params[:id])
+    @rules_page = RulesPage.find(params[:id])
   end
 
   def create
-    @rules_page = RulesPage.new(rules_page_params);
+    @rules_page = RulesPage.new(rules_page_params)
 
     if @rules_page.save
       redirect_to @rules_page
@@ -23,9 +24,19 @@ class RulesPagesController < ApplicationController
       render 'new'
     end
   end
-end
 
-private
-  def rules_page_params
-    params.require(:rules_page).permit(:title, :text)
+  def update
+    @rules_page = RulesPage.find(params[:id])
+
+    if @rules_page.update(rules_page_params)
+      redirect_to @rules_page
+    else
+      render 'edit'
+    end
   end
+
+  private
+    def rules_page_params
+      params.require(:rules_page).permit(:title, :text)
+    end
+end
