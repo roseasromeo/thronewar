87a2390f77d9892d3bcb6efe035ec29b56ec3b33
Red Lhota <rose.as.romeo@gmail.com>
2015-12-18 23:56:05 -0600

    Created User model. Began adding authentication methods to controllers.

---
+++ cms/db/migrate/20151219053828_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email, unique: true, null: false
+      t.string :name, unique: true, null: false
+      t.string :password_hash, null: false
+      
+      t.integer :user_type, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
