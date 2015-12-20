71e5d3c0fb15467f2d77606eb591019cd2ea3387
Red Lhota <rose.as.romeo@gmail.com>
2015-12-19 18:28:42 -0600

    Added comments model.

--- cms/db/schema.rb
+++ cms/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219053828) do
+ActiveRecord::Schema.define(version: 20151219232819) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.integer  "rules_page_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "comments", ["rules_page_id"], name: "index_comments_on_rules_page_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "rules_pages", force: :cascade do |t|
     t.string   "name"
