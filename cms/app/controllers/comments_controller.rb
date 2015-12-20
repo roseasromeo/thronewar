6fd3cdf6ee67dbd610e18e0b07892804610150f2
Red Lhota <rose.as.romeo@gmail.com>
2015-12-20 13:57:58 -0600

    Added css to edit and show pages for RulesPage. Fixed Comment display table.

--- cms/app/controllers/comments_controller.rb
+++ cms/app/controllers/comments_controller.rb
@@ -7,6 +7,13 @@ class CommentsController < ApplicationController
     redirect_to rules_page_path(@rules_page)
   end
 
+  def destroy
+    @rules_page = RulesPage.find_by_slug(params[:rules_page_id])
+    @comment = @rules_page.comments.find(params[:id])
+    @comment.destroy
+    redirect_to rules_page_path(@rules_page)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body)
