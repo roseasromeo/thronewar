fef64308459245b3e95be17159ca01c21506161e
Red Lhota <rose.as.romeo@gmail.com>
2015-12-27 16:34:15 -0500

    Added FinalCharacter, CharacterSystem, and Flaw models.

---
+++ cms/app/models/final_character.rb
@@ -0,0 +1,15 @@
+class FinalCharacter < ActiveRecord::Base
+  belongs_to :character_system
+  belongs_to :user
+  has_many :ranks
+  has_many :tools
+  has_many :regencies
+  belongs_to :flaw1, :class_name => 'Flaw', :foreign_key => 'flaw1_id'
+  belongs_to :flaw2, :class_name => 'Flaw', :foreign_key => 'flaw2_id'
+
+
+  enum approval: [:not_submitted, :submitted, :rejected, :approved]
+
+  validates_presence_of :game, :user, :approval
+
+end
