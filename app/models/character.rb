0ee16c3679e1269916fbe54beb3a675c5d996305
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2016-01-03 20:51:57 -0800

    Fixed games controller issue where it forces rank assignment in case of early auction closure.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
-  has_many :char_rounds
+  has_many :char_rounds, dependent: :destroy
   has_many :pledges, through: :char_rounds
 
   validates_presence_of :pseudonym, :points_spent
