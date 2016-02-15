2520d819f10012df53468c7cb4d833d670941432
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2016-02-14 22:36:04 -0800

    Finished sortable bid tables.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
-  def sortable(column, title = nil)
+  def sortable(column, title = nil, display_toggle = false)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    display_toggle = display_toggle == true ? true : false
+    link_to title, {:sort => column, :direction => direction, :display_toggle => display_toggle}, {:class => css_class}
   end
 end
