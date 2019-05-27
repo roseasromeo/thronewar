ddc1ee25b9d0c04576e5e3fd87762a1135467320
Red Lhota <rose.as.romeo@gmail.com>
2019-05-26 21:36:38 -0700

    upgraded to Rails 5.2

--- db/migrate/20160130172451_create_link_indices.rb
+++ db/migrate/20160130172451_create_link_indices.rb
@@ -1,4 +1,4 @@
-class CreateLinkIndices < ActiveRecord::Migration
+class CreateLinkIndices < ActiveRecord::Migration[5.2]
   def change
     create_table :link_indices do |t|
       t.string :title, null: false, unique: true
