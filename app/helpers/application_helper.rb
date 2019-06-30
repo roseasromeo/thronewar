142692fe2f781f216b46c28612187dee438532b6
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2019-06-30 14:14:59 -0700

    final character notices now legible

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,19 @@ module ApplicationHelper
     content_for(:title){ title }
     content_tag(:h1, title, class: "page_title" )
   end
+
+  def flash_message(type, text)
+    flash[type] ||= []
+    flash[type] << text
+  end
+
+  def render_flash
+    rendered = []
+    flash.each do |type, messages|
+      messages.each do |m|
+        rendered << render(:partial => 'partials/flash', :locals => {:type => type, :message => m}) unless m.blank?
+      end
+    end
+    safe_join(rendered, "<br>".html_safe)
+  end
 end
